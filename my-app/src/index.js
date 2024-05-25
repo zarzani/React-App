7587d053da81d8c3f1aa94c88e05ebeef9704b67
zarzani <zahraarzani@gmail.com>
2024-05-25 09:51:40 +0330

    write h1 and h2

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -8,10 +8,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <div className="App">
+      <h1>Zahra Arzani</h1>
+
+      <h3>weather App</h3>
+      <WeatherSearch />
+      </div>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
